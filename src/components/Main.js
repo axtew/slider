11e0c98c78ae3be2951634995142dc4eefc3294a
Artur Ovcharenko <stevvy1122@gmail.com>
2018-07-08 18:58:08 +0300

    add slider

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,8 @@ import Col from 'react-bootstrap/lib/Col';
 
 import styled from 'styled-components';
 
+import Slider from './Slider';
+
 const Repair = styled.div`
   font-size: 40px;
   color: #fff;
@@ -57,7 +59,7 @@ class Main extends React.Component {
           </SendBtn>
         </Col>
         <Col lg={6} lgOffset={1}>
-          
+          <Slider />
         </Col>
       </Row>
     )
